d542ce63fd08003db52474fe69f609cc5a977fe9
Curtis Lassam <throwaway@gooble.email>
2023-10-07 16:30:37 -0700

    behold

--- server.py
+++ server.py
@@ -1,13 +1,62 @@
 """
     TODO: doc
 """
+import os
+from contextlib import asynccontextmanager
+import asyncio
+from urllib.parse import urlparse
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
+from redis import asyncio as redis
 
-app = FastAPI()
-BASE_URL: str = "http://locahost:8000"
+import silly
 
+PORT: int = int(os.getenv("PORT") or 8000)
+BASE_URL: str = os.getenv("BASE_URL") or f"http://localhost:{PORT}"
+REDIS_URL: str = os.getenv("REDIS_URL") or "redis://localhost:6379"
+
+# parse redis url into a host and port and password (if applicable)
+redis_url = urlparse(REDIS_URL)
+redis_host = redis_url.hostname
+redis_port = redis_url.port
+redis_password = redis_url.password
+
+redis_pool = None
+application_services = {}
+
+## this is maybe necessary on windows
+## asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+async def test_redis_connection(redis_pool: redis.ConnectionPool):
+    """
+    Test that we can connect to Redis.
+    We always do this right after boot-up: if we can't connect to Redis,
+        it would be better to fail up-front.
+    """
+    redis_client = await redis.Redis(
+        connection_pool=redis_pool)
+    await redis_client.set("test", "test")
+    test = await redis_client.get("test")
+    assert test == "test"
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    pool = redis.ConnectionPool.from_url(REDIS_URL, decode_responses=True)
+
+    await test_redis_connection(pool)
+    application_services['redis_pool'] = pool
+    yield
+    # giant habbo raid july 12th at 10:00pm
+    await pool.close()
+
+app = FastAPI(lifespan=lifespan)
+
+def redis_key(id: str) -> str:
+    """
+    Return the Redis key for a given ID.
+    """
+    return f"url:{id}"
 
 class ShortenRequest(BaseModel):
     """
@@ -15,23 +64,42 @@ class ShortenRequest(BaseModel):
     """
     url: str
 
-
-@app.post("/url/shorten")
-async def url_shorten(request: ShortenRequest):
-    """
-    Given a URL, generate a short version of the URL that can be later resolved to the originally
-    specified URL.
-    """
-    short_url = "new-short-url"
-    return {"short_url": f"{BASE_URL}/r/{short_url}"}
-
-
-class ResolveRequest(BaseModel):
+class ShortenResponse(BaseModel):
     """
     beef borf
     """
     short_url: str
 
+@app.post("/url/shorten")
+async def url_shorten(request: ShortenRequest):
+    """
+    Given a URL, generate a short version of the URL that can be
+    later resolved to the originally specified URL.
+    """
+    short_id = silly.name(slugify=True)
+    redis_client = redis.Redis(connection_pool=redis_pool)
+    print("shoretning::::")
+    print(request.url)
+    await redis_client.set(redis_key(short_id), request.url)
+    return {
+        "short_url": f"{BASE_URL}/r/{short_id}",
+        "short_id": short_id
+    }
+
+@app.post("/url/longen")
+async def url_longen(request: ShortenRequest):
+    """
+    Given a URL, generate a long version of the URL that can be
+    later resolved to the originally specified URL.
+    """
+    long_id = silly.sentence(slugify=True)
+    redis_client = redis.Redis(connection_pool=redis_pool)
+    await redis_client.set(redis_key(long_id), request.url)
+
+    return {
+        "long_url": f"{BASE_URL}/r/{long_id}",
+        "long_id": long_id
+    }
 
 @app.get("/r/{short_url}")
 async def url_resolve(short_url: str):
@@ -39,7 +107,15 @@ async def url_resolve(short_url: str):
     Return a redirect response for a valid shortened URL string.
     If the short URL is unknown, return an HTTP 404 response.
     """
-    return RedirectResponse("http://original/url")
+    # the default response is just a redirect to the base URL
+    redis_client = redis.Redis(connection_pool=redis_pool,
+                                decode_responses=True)
+    redirect_target = await redis_client.get(redis_key(short_url))
+    print("redorircotong: :: >")
+    print(redirect_target)
+    if not redirect_target:
+        return RedirectResponse(BASE_URL)
+    return RedirectResponse(redirect_target)
 
 
 @app.get("/")
--- tests.py
+++ tests.py
@@ -1,9 +1,64 @@
+"""
+    When the tests pass, the test is putatively complete.
+"""
+import json
 import requests
 
 def test_index():
-	r = requests.get('http://localhost:8000/')
-	assert r.status_code == 200
+    """
+        hello, world
+    """
+    r = requests.get('http://localhost:8000/', timeout=500)
+    assert r.status_code == 200
 
-#def test_post():
-#	r = requests.post('http://localhost:8000/url/shorten')
-#	assert r.status_code == 200
+def test_post():
+    """
+        Test that shortening a URL returns a 200 response.
+    """
+    url_to_shorten = "https://www.google.com"
+
+    r = requests.post('http://localhost:8000/url/shorten',
+        data=json.dumps({'url': url_to_shorten}), timeout=500)
+
+    print(r.text)
+    assert r.status_code == 200
+
+def test_longen_post():
+    """
+        Test that longening a URL returns a 200 response.
+    """
+    url_to_longen = "https://www.google.com"
+
+    r = requests.post('http://localhost:8000/url/longen',
+        data=json.dumps({'url': url_to_longen}), timeout=500)
+
+    print(r.text)
+    assert r.status_code == 200
+
+def test_redirect():
+    """
+        Test that the response from the redirect endpoint is, in fact, a redirect.
+    """
+    r = requests.get('http://localhost:8000/r/abc123', timeout=500,
+                        allow_redirects=False)
+    assert r.status_code == 307
+    # test that the location header is set
+    assert r.headers['Location'] == "http://localhost:8000"
+
+def test_that_the_redirect_logic_works_the_way_we_expect_it_to():
+    """
+        When we give the redirect endpoint a shorten command,
+            it should return a code that we can use to
+            redirect to the original URL.
+    """
+    url_to_shorten = "https://wwwww.gooble.email"
+
+    r = requests.post('http://localhost:8000/url/shorten',
+        data=json.dumps({'url': url_to_shorten}), timeout=500)
+
+    short_url = r.json()['short_url']
+    assert short_url is not None
+
+    r = requests.get(short_url, timeout=500, allow_redirects=False)
+    assert r.status_code == 307
+    assert r.headers['Location'] == url_to_shorten

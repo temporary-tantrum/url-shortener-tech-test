fa0f5f93c640bf084feef84343ebeae8921cfb59
Curtis Lassam <throwaway@gooble.email>
2023-10-07 18:18:37 -0700

    whoops, I thought that timeout was in milliseconds

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ def test_index():
     """
         hello, world
     """
-    r = requests.get('http://localhost:8000/', timeout=500)
+    r = requests.get('http://localhost:8000/', timeout=1)
     assert r.status_code == 200
 
 def test_post():
@@ -18,7 +18,7 @@ def test_post():
     url_to_shorten = "https://www.google.com"
 
     r = requests.post('http://localhost:8000/url/shorten',
-        data=json.dumps({'url': url_to_shorten}), timeout=500)
+        data=json.dumps({'url': url_to_shorten}), timeout=1)
 
     print(r.text)
     assert r.status_code == 200
@@ -30,7 +30,7 @@ def test_longen_post():
     url_to_longen = "https://www.google.com"
 
     r = requests.post('http://localhost:8000/url/longen',
-        data=json.dumps({'url': url_to_longen}), timeout=500)
+        data=json.dumps({'url': url_to_longen}), timeout=1)
 
     print(r.text)
     assert r.status_code == 200
@@ -39,7 +39,7 @@ def test_redirect():
     """
         Test that the response from the redirect endpoint is, in fact, a redirect.
     """
-    r = requests.get('http://localhost:8000/r/abc123', timeout=500,
+    r = requests.get('http://localhost:8000/r/abc123', timeout=1,
                         allow_redirects=False)
     assert r.status_code == 307
     # test that the location header is set
@@ -54,11 +54,11 @@ def test_that_the_redirect_logic_works_the_way_we_expect_it_to():
     url_to_shorten = "https://wwwww.gooble.email"
 
     r = requests.post('http://localhost:8000/url/shorten',
-        data=json.dumps({'url': url_to_shorten}), timeout=500)
+        data=json.dumps({'url': url_to_shorten}), timeout=1)
 
     short_url = r.json()['short_url']
     assert short_url is not None
 
-    r = requests.get(short_url, timeout=500, allow_redirects=False)
+    r = requests.get(short_url, timeout=1, allow_redirects=False)
     assert r.status_code == 307
     assert r.headers['Location'] == url_to_shorten
